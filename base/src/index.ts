0e851140c0af7da35f2dba2518749ea8d1e954c2
Lou Cyx <git@lou.cx>
2022-03-03 13:57:10 -0800

    :arrow_up: dependency upgrade. :rotating_light: add missing `@returns` in `index.ts`.

--- base/src/index.ts
+++ base/src/index.ts
@@ -7,5 +7,6 @@
  * console.log(greeting("Vangware")); // Hello, Vangware!
  * ```
  * @param name Name to be greeted.
+ * @returns A greeting message.
  */
 export const greet = (name: string) => `Hello, ${name}!`;
