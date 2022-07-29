84ecc892b316f42420109fc1a8de079b3b058635
Lou Cyx <git@lou.cx>
2022-07-29 01:58:50 -0700

    :arrow_up: dependency upgrade. :building_construction: change template with better TypeDoc config.

--- src/templates/typedoc.json.ts
+++ src/templates/typedoc.json.ts
@@ -2,12 +2,14 @@ import type { TemplateData } from "../types/TemplateData.js";
 import { userClean } from "../userClean.js";
 
 export default ({ name }: TemplateData) =>
-	JSON.stringify({
-		cleanOutputDir: false,
-		cname: `${userClean(name)}.vangware.com`,
-		customCss: "./node_modules/@vangware/configs/typedoc.css",
-		entryPoints: ["./src/index.ts"],
-		gitRevision: "main",
-		hideGenerator: true,
-		logLevel: "Warn",
-	});
+	JSON.stringify(
+		{
+			$schema: "https://typedoc.org/schema.json",
+			cname: `${userClean(name)}.vangware.com`,
+			entryPoints: ["./lib"],
+			extends: ["./node_modules/@vangware/configs/typedoc.json"],
+			name,
+		},
+		undefined,
+		"	",
+	);
