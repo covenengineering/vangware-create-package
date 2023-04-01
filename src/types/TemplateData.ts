2195fb560cb92e48c0a5357ac6cf04a9578cdb74
Luke Shiru <hi@luke.sh>
2023-04-01 00:56:57 -0700

    ⬆️ dependency upgrade.

--- src/types/TemplateData.ts
+++ src/types/TemplateData.ts
@@ -1,6 +1,5 @@
 import type { ReadOnlyRecord } from "@vangware/types";
 import type { Answers } from "./Answers.js";
 
-export type TemplateData = Answers & {
-	readonly devDependencies: ReadOnlyRecord<string, string>;
-};
+export type TemplateData = Answers &
+	ReadOnlyRecord<"devDependencies", ReadOnlyRecord<string, string>>;
